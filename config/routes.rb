9b7c09a92f067abbf8ad1dcc2635d8bf7ad6f9cb
Jefferson Choi <jeffersonchoi@gmail.com>
2015-05-30 14:03:11 -0700

    added user authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get "users" => "users#index"
+  get "users/new" => "users#new" 
+  post "users" => "users#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
